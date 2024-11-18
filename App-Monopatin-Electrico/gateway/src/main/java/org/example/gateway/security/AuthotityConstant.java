e87d729ddfbf1f0c3c8b69f7721b4427cfde068e
Romero,Luciana <lu.yael.r@gmail.com>
2024-11-18 12:22:43 -0300

    controles de seguridad, accesos permitidos y con restriccion

--- App-Monopatin-Electrico/gateway/src/main/java/org/example/gateway/security/AuthotityConstant.java
+++ App-Monopatin-Electrico/gateway/src/main/java/org/example/gateway/security/AuthotityConstant.java
@@ -5,6 +5,6 @@ public final class AuthotityConstant {
     private AuthotityConstant() {}
 
     public static final String _ADMIN = "ADMIN";
-    public static final String _PROFESOR = "PROFESOR";
-    public static final String _ALUMNO = "ALUMNO";
+    public static final String _MANTENIMIENTO = "MANTENIMIENTO";
+    public static final String _USUARIO = "USUARIO";
 }
